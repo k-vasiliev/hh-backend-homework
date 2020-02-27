669668430b1197a4299445e34936e7322d70b663
Nikolay Petryashev <petryashev.n.o@gmail.com>
2020-02-27 17:13:46 +0300

    add app config

--- backend/src/main/java/Application.java
+++ backend/src/main/java/Application.java
@@ -1,25 +1,11 @@
-import ru.hh.school.models.Resume;
-import ru.hh.school.models.User;
-import ru.hh.school.services.UserService;
-
-import java.sql.SQLException;
+import ru.hh.nab.starter.NabApplication;
+import ru.hh.school.config.Config;
 
 public class Application {
 
-    public static void main(String[] args) throws SQLException {
-
-//        UserService userService = new UserService();
-//        User user = new User("Masha", 0);
-//        userService.saveNew(user);
-//        Resume res1 = new Resume("Ferrari", "red");
-//        user.addResume(res1);
-//        Resume res2 = new Resume("Ford", "black");
-//        res2.setUser(user);
-//        user.addResume(res2);
-//        userService.updateUser(user);
-//        user.setName("Sasha");
-//        userService.updateUser(user);
-//        userService.deleteUser(user);
+    public static void main(String[] args) {
+        NabApplication.builder()
+                .configureJersey().bindToRoot()
+                .build().run(Config.class);
     }
-
 }
---
+++ backend/src/main/java/ru/hh/school/config/Config.java
@@ -0,0 +1,15 @@
+package ru.hh.school.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import ru.hh.nab.hibernate.NabHibernateProdConfig;
+import ru.hh.nab.starter.NabProdConfig;
+
+@Configuration
+@Import({NabProdConfig.class,
+        NabHibernateProdConfig.class,
+        BaseConfig.class})
+@ComponentScan("ru.hh.school")
+public class Config {
+}
