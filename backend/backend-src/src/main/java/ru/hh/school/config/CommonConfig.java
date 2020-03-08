dd95321186e57f9ff09571377943c1a8a0e0bcb5
PavelMeld <pavel.meld@gmail.com>
2020-03-08 20:20:14 +0300

    Adding Hibernate bean

--- backend/backend-src/src/main/java/ru/hh/school/config/CommonConfig.java
+++ backend/backend-src/src/main/java/ru/hh/school/config/CommonConfig.java
@@ -1,5 +1,12 @@
 package ru.hh.school.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.BootstrapServiceRegistryBuilder;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import ru.hh.nab.hibernate.NabSessionFactoryBean;
+import ru.hh.nab.hibernate.NabSessionFactoryBuilderFactory;
+import ru.hh.nab.hibernate.transaction.DataSourceContextTransactionManager;
 import ru.hh.school.controller.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +14,13 @@ import org.springframework.context.annotation.Import;
 import ru.hh.nab.hibernate.MappingConfig;
 import ru.hh.nab.starter.NabCommonConfig;
 import ru.hh.school.controller.*;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Properties;
 
 @Configuration
 @Import({
@@ -21,9 +35,52 @@ import ru.hh.school.controller.*;
 public class CommonConfig {
 
     @Bean
+    DataSource dataSource() {
+        DataSource hikariDataSource = new HikariDataSource() {
+            @Override
+            public Connection getConnection() throws SQLException {
+                return connection;
+            }
+        };
+        return hikariDataSource;
+    }
+
+    @Bean
     public MappingConfig mappingConfig() {
         MappingConfig mappingConfig = new MappingConfig();
         mappingConfig.addPackagesToScan("ru/hh/school/entity");
         return mappingConfig;
     }
+
+
+    @Bean
+    NabSessionFactoryBean.ServiceSupplier<?> nabSessionFactoryBuilderServiceSupplier() {
+        return new NabSessionFactoryBean.ServiceSupplier<NabSessionFactoryBuilderFactory.BuilderService>() {
+            @Override
+            public Class<NabSessionFactoryBuilderFactory.BuilderService> getClazz() {
+                return NabSessionFactoryBuilderFactory.BuilderService.class;
+            }
+
+            @Override
+            public NabSessionFactoryBuilderFactory.BuilderService get() {
+                return new NabSessionFactoryBuilderFactory.BuilderService();
+            }
+        };
+    }
+
+    @Bean
+    DataSourceContextTransactionManager transactionManager(SessionFactory sessionFactory, DataSource routingDataSource) {
+        HibernateTransactionManager simpleTransactionManager = new HibernateTransactionManager(sessionFactory);
+        simpleTransactionManager.setDataSource(routingDataSource);
+        return new DataSourceContextTransactionManager(simpleTransactionManager);
+    }
+
+    @Bean
+    NabSessionFactoryBean sessionFactoryBean(DataSource dataSource, NabSessionFactoryBean.ServiceSupplier supplier) throws IOException {
+        var props = new Properties();
+        props.load(CommonConfig.class.getResourceAsStream("src/etc/hibernate.properties"));
+        return new NabSessionFactoryBean(dataSource, props,
+                new BootstrapServiceRegistryBuilder(), List.of(supplier), List.of());
+    }
+
 }
