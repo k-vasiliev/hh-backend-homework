b7fa54041f7acbdabd5fc7ca2e733457559a0f43
Adam Mizaushev <adammizaushev00@gmail.com>
2020-03-12 23:33:57 +0300

    [Fix]: ResumeDto.Id, though unseen, is needed for Negotiations

--- backend/src/main/java/ru/hh/homework/at_least_some_backend/dto/query/HHQueryResumeDto.java
+++ backend/src/main/java/ru/hh/homework/at_least_some_backend/dto/query/HHQueryResumeDto.java
@@ -1,19 +1,20 @@
 package ru.hh.homework.at_least_some_backend.dto.query;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import ru.hh.homework.at_least_some_backend.Utils;
 import ru.hh.homework.at_least_some_backend.entity.HHResume;
 
-import java.time.format.DateTimeFormatter;
-
 public class HHQueryResumeDto
 {
+    private Long id;
     private String title;
     @JsonProperty("applicant")
     private HHQueryUserDto user;
     @JsonProperty("dateCreate")
     private String createdAt;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
@@ -30,6 +31,7 @@ public class HHQueryResumeDto
 
         var dto = new HHQueryResumeDto();
 
+        dto.setId(entity.getId());
         dto.setTitle(entity.getTitle());
         dto.setUser(HHQueryUserDto.fromEntity(entity.getUser()));
 
