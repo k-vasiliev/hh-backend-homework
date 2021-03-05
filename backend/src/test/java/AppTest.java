7ce27f27d7a65a7240f4dc5900f475d722655fa1
Sergey Makovkin <mackovckin.sergei@yandex.ru>
2021-03-05 18:19:19 +0300

    switching to hometask

--- backend/src/test/java/AppTest.java
+++ backend/src/test/java/AppTest.java
@@ -1,40 +1,14 @@
-import com.opentable.db.postgres.embedded.EmbeddedPostgres;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.test.context.ContextConfiguration;
 import ru.hh.nab.starter.NabApplication;
 import ru.hh.nab.testbase.NabTestBase;
-import ru.hh.nab.testbase.postgres.embedded.EmbeddedPostgresDataSourceFactory;
 
 import javax.ws.rs.core.Response;
-
-import java.io.IOException;
-import java.sql.SQLException;
-
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 @ContextConfiguration(classes = AppTestConfig.class)
 public class AppTest extends NabTestBase {
-
-  protected static EmbeddedPostgres pg;
-  protected static SessionFactory sessionFactory;
-
-  @BeforeClass
-  public static void setUp() throws IOException, SQLException {
-    pg = EmbeddedPostgresDataSourceFactory.getEmbeddedPostgres();
-    StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-            .loadProperties("hibernate.test.properties").build();
-    MetadataSources metadataSources = new MetadataSources(serviceRegistry);
-    //ENTITY_CLASSES_REGISTRY.forEach(metadataSources::addAnnotatedClass);
-
-    sessionFactory = metadataSources.buildMetadata().buildSessionFactory();
-  }
 
   @Override
   protected NabApplication getApplication() {
