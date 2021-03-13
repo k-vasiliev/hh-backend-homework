7db4504b38f5c5ccc530fc613c06d2db7ec39964
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2021-03-13 11:07:37 +0300

    update sql script

--- backend/init.sql
+++ backend/init.sql
@@ -15,14 +15,21 @@ CREATE TABLE employer
     views_count INTEGER
 );
 
+CREATE TABLE vac_employer
+(
+    id          INTEGER PRIMARY KEY,
+    name        VARCHAR(200)
+);
+
 CREATE TABLE vacancy
 (
     id          INTEGER PRIMARY KEY,
     name        VARCHAR(200),
     date_create DATE,
     area_id     INTEGER NOT NULL REFERENCES area(id),
-    salary      REAL,
-    created_at  DATE,
-    employer_id INTEGER NOT NULL REFERENCES employer(id),
-    views_count INTEGER
+    salary      INTEGER,
+    created_at  TEXT,
+    employer_id INTEGER NOT NULL REFERENCES vac_employer(id),
+    views_count INTEGER,
+    comment     TEXT
 );
