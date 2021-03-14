18a98dc95f5cbccaa3c0b8ea9741796a134e4d73
Abdulov Azad <abdulov.azad@yandex.ru>
2021-03-14 10:22:06 +0300

    add sql db creation scripts

---
+++ backend/init.sql
@@ -0,0 +1,30 @@
+create table if not exists area (
+  area_id serial primary key,
+  area_name varchar(100) not null
+);
+
+create table if not exists favorite_employer (
+  employer_id serial primary key,
+  company_name varchar(100) not null,
+  company_description text,
+  area_id int,
+  creation_time timestamptz,
+  comment varchar(100),
+  views_count int
+);
+
+create table if not exists favorite_vacancy (
+  vacancy_id serial primary key,
+  vacancy_name varchar(100) not null,
+  archiving_time timestamptz default NOW(),
+  area_id int,
+  compensation_from int,
+  compensation_to int,
+  compensation_gross boolean,
+  compensation_currency varchar(3) -- poor cryptocurrencies should follow ISO convention
+  creation_time timestamptz,
+  employer_id int not null,
+  foreign key (employer_id) references favorite_employer(employer_id),
+  views_count int,
+  comment varchar(100)
+);
